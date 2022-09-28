0d00ca8a8d145d626c8ce8b6b731336e0bdc4e01
NguyenQMinh01 <minhmon112233@gmail.com>
2022-09-28 22:56:54 +0700

    pushtask5

--- Task5/task5.rb
+++ Task5/task5.rb
@@ -1,10 +1,10 @@
-#clean code
+#clean code one line case true
 a = [ 5, 22, 26, 1, 6, -1, 10, 8]
 b = [1, 6, -1, 10]
 puts a.each_cons(b.length).include?(b) #==> true
 
 
-#clean code
+#clean code one line case true
 a = [ 5, 22, 26, 6, 1, -1, 10, 8]
 b = [1, 6, -1, 10]
 puts a.each_cons(b.length).include?(b) #==> false
@@ -12,10 +12,10 @@ puts a.each_cons(b.length).include?(b) #==> false
 
 
 
-#performance
+#performance case true with method check 
 a = [5, 22, 26, 1, 6, -1, 10, 8]
 b = [1, 6, -1, 10]
 def check(array, brray)
-    puts array.each_cons( brray.length).include?( brray)
+    puts array.each_cons(brray.length).include?(brray)
 end
 check(a,b)
