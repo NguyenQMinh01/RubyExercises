d52a644eb2e9116c678c8ada94df26e64b9ee8fb
NguyenQMinh01 <minhmon112233@gmail.com>
2022-11-01 09:36:32 +0700

    push test7cah1

--- Task7/test7cach1.rb
+++ Task7/test7cach1.rb
@@ -4,23 +4,23 @@ def findTriplets(array, numbers, target_sum)
  
     for i in (0..numbers - 1)
         
-        l = i + 1
-        r = numbers - 1
+        left = i + 1
+        right = numbers - 1
         x = arraysort[i]
-        while (l < r) 
-            if (x + arraysort[l] + arraysort[r] == target_sum) 
+        while (left < right) 
+            if (x + arraysort[left] + arraysort[right] == target_sum) 
                 
                
-                print "[#{x} #{arraysort[l]} #{arraysort[r]}]"
-                l = l + 1
-                r = r - 1
+                print "[#{x} #{arraysort[left]} #{arraysort[right]}]"
+                left = left + 1
+                right = right - 1
              
         
-            elsif (x + arraysort[l] + arraysort[r] < target_sum)
-                l = l + 1
+            elsif (x + arraysort[left] + arraysort[right] < target_sum)
+                left = left + 1
  
             else
-                r = r - 1
+                right = right - 1
             end
         end
     end
