a793c3032f0118fcf17a51a00be61fd4ecae6bde
NguyenQMinh01 <minhmon112233@gmail.com>
2022-11-13 23:25:11 +0700

    push task7 again

--- Task7/test7cach1.rb
+++ Task7/test7cach1.rb
@@ -1,7 +1,10 @@
+$count = 0
+
+
 def findTriplets(array, numbers, target_sum)
- 
+  
     arraysort =  array.sort()
- 
+    
     for i in (0..numbers - 1)
         
         left = i + 1
@@ -11,10 +14,10 @@ def findTriplets(array, numbers, target_sum)
             if (x + arraysort[left] + arraysort[right] == target_sum) 
                 
                
-                print "[#{x} #{arraysort[left]} #{arraysort[right]}]"
+                print "[#{x} #{arraysort[left]} #{arraysort[right]}]\n"
                 left = left + 1
                 right = right - 1
-             
+                $count = 1
         
             elsif (x + arraysort[left] + arraysort[right] < target_sum)
                 left = left + 1
@@ -24,9 +27,20 @@ def findTriplets(array, numbers, target_sum)
             end
         end
     end
+  
+   
 end
 # Driver code
-array = [12, 3, 1, 2, -6, 5, -8, 6]
+array = [12, 3, 5, 8, 9, 18, -6,-8, 5, 6]
 target_sum = 0
 numbers = array.size
 findTriplets(array, numbers, target_sum)
+
+def check
+    if( $count == 1 )
+      return 
+    else
+      print "[]"
+    end
+  end
+check()
--- Task7/test7cach2.rb
+++ Task7/test7cach2.rb
@@ -1,18 +1,35 @@
+VALUE = 0
+$count = 0
 target_sum = 0 
-numbers = [12,3,1,2,-6,5,-8,6]
+
+
+numbers = [12,3,2,-6,5]
 
 def bai7(numbers,target, array=[])
     sum = array.sum
     
-    print "#{array}" if (sum == target && array.size == 3)
-   
-    (0..(numbers.length - 1)).each do |i|
+    if (sum == target && array.size == 3)
+      print "#{array}" 
+      $count = 1 
+    end
+    
+
+    (VALUE..(numbers.length - 1)).each do |i|
       value = numbers[i]
       remaining = numbers.drop(i+1)
       bai7(remaining, target, array + [value])
-      print array + [value]
     end
+
 end
 
 
 bai7(numbers,target_sum)
+
+def check
+  if( $count == 1 )
+    return 
+  else
+    print "[]"
+  end
+end
+check()
